1d5201aeca85472c0af7a5f426fdd21c575608c6
pushpendra0230 <153480201+pushpendra0230@users.noreply.github.com>
2024-10-09 16:56:35 +0530

    javascript

---
+++ 2p.js
@@ -0,0 +1,10 @@
+var a=5;
+// var i; //global
+let i=8;
+// loop using a 
+for(let i=0;i<a;i++)
+{
+    console.log(i)
+}
+console.log("after completetion of the loop",i);
+console.log(i+2); 
---
+++ firstprogram.js
@@ -0,0 +1,6 @@
+var a=5;
+for(var i=0;i<a;i++)
+{
+    console.log(i)
+}
+console.log(a);
