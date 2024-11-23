6050b0834a62777974807f725024b726b93a261d
pushpendra0230 <153480201+pushpendra0230@users.noreply.github.com>
2024-11-23 18:35:58 +0530

    js

---
+++ 9.js
@@ -0,0 +1,5 @@
+let arr =[1, 2, 3, 4, 5];
+     let even_num = arr.filter((num) => num %2 ==0) ;
+     let even_num_sqar = even_num.map((num) =>num*num);
+     console.log(even_num);
+     console.log(even_num_sqar);
