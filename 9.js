1ef70cebd14836a7b0763338d38b9f08b4159955
pushpendra0230 <153480201+pushpendra0230@users.noreply.github.com>
2024-12-05 15:14:22 +0530

    js

--- 9.js
+++ 9.js
@@ -111,3 +111,77 @@
 //     (restaurant)=>restaurant.distance<=5&&restaurant.rating>=4
 //   );
 //   console.log(restaurant);
+
+
+
+// let restaurants=[
+//     {name:"Romantic Rooftop",distance:3,rating:4.5},
+//     {name:"Cozy Corner",distance:6,rating:4.2},
+//     {name:"Candlelight Cafe",distance: 2,rating:3.9},
+//     { name: "Starry Sky Diner", distance: 4,rating:4.8},
+//   ];
+// let restaurantname=restaurants.map((restaurant)=>restaurant.name);
+// console.log(restaurantname);
+
+
+
+// let restaurants=[
+//     {name:"Romantic Rooftop",distance:3,rating:4.5},
+//     {name:"Cozy Corner",distance:6,rating:4.2},
+//     {name:"Candlelight Cafe",distance: 2,rating:3.9},
+//     { name: "Starry Sky Diner", distance: 4,rating:4.8},
+//   ];
+// const firstOption = restaurants.find(
+//     (restaurant)=>restaurant.distance<=5&&restaurant.rating>=4
+//   );
+//   console.log(firstOption);
+
+
+// let letters=[
+//     ["I","will","love","you","forever"],
+//     ["You","are","the","love","of","my","life"],
+//     ["Love","is","what","binds","us"],
+//   ];
+//   let love=letters.every((letter)=>
+//     letter.some((word)=>word.toLowerCase()==="love")
+//   );
+//   console.log(love);
+
+
+// let letters=[
+//     ["I","will","love","you","forever"],
+//     ["You","are","the","love","of","my","life"],
+//     ["Love","is","what","binds","us"],
+//   ];
+//   let forever=letters.some((letter)=>letter.includes("forever"));
+// console.log(forever);
+
+// let movies=[
+//     {title:"Love Actually",genre:"romance",duration:135},
+//     {title:"Inception",genre:"scifi",duration:148},
+//     {title:"The Notebook",genre:"romance",duration:123},
+//     {title:"Titanic",genre:"romance",duration:195},
+//   ];
+// let titles=movies.map(movie=>movie.title);
+// console.log(titles);
+
+// let gift=[
+//   {name:"Teddy Bear",price:800,popularity:4.5},
+//   {name:"Perfume",price:1500,popularity:4.7},
+//   {name:"Photo Frame",price:500,popularity:3.8},
+//   {name:"Necklace",price:4500,popularity:4.9},
+// ];
+// let all=gift.every(gift=>gift.price<5000);
+// console.log(all);
+
+// let songs=[
+//   {title:"Perfect",artist:"Ed Sheeran",duration:263},
+//   {title:"All of Me",artist:"John Legend",duration:269},
+//   {title:"A Thousand Years",artist:"Christina Perri",duration:295},
+// ];
+// const convertedDurations = songs.map(song => {
+//   const minutes = Math.floor(song.duration / 60);
+//   const seconds = song.duration % 60;
+//   return `${minutes}:${seconds.toString(),(2, "0")}`;
+// });
+// console.log("Converted durations:", convertedDurations);
