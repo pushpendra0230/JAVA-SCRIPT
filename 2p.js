65fee30b6756c2d777ae0f76ba3f68ae286b3c0a
pushpendra0230 <153480201+pushpendra0230@users.noreply.github.com>
2024-12-16 16:03:42 +0530

    new

--- 2p.js
+++ 2p.js
@@ -1,10 +1,31 @@
-var a=5;
-// var i; //global
-let i=8;
-// loop using a 
-for(let i=0;i<a;i++)
-{
-    console.log(i)
-}
-console.log("after completetion of the loop",i);
-console.log(i+2);
+// // var a=5;
+// // // var i; //global
+// // let i=8;
+// // // loop using a 
+// // for(let i=0;i<a;i++)
+// // {
+// //     console.log(i)
+// // }
+// // console.log("after completetion of the loop",i);
+// // console.log(i+2);
+
+
+// // let arr = [{ id: 1 }, { id: 2 }];
+// // let obj = arr.reduce((acc, item) => 
+// //     {
+// //     acc[item.id] = item;
+// //     return acc;
+// // }, {});
+// // console.log(obj);
+
+
+// // const numbers = [3, 6, 2, 8, 5];
+// // const max=numbers.reduce((max,num)=>(num>max?num:max),numbers[0]);
+// // console.log(max);
+
+
+
+// let arr = [[1,2],[3,4]];
+// let n = arr.map(i =>{return i}).flat(Infinity)
+// console.log(n);
+
